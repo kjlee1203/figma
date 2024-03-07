91ddf43b1d8058ad22a0261ca67afb78403da44d
kjlee1203 <kjuni1@naver.com>
2024-03-07 17:23:50 +0900

    add background

--- login2/src/Template5Login.jsx
+++ login2/src/Template5Login.jsx
@@ -1,4 +1,5 @@
 import logoImg from "./assets/fba_logo.png";
+import bgImg from "./assets/bg.png";
 import StartTrialButton from  "./components/StartTrialButton.jsx";
 import './Template5Login.css'
 
@@ -6,7 +7,8 @@ function Template5Login() {
   return (
     <div className="template-5login">
       <section className="rectangle-parent">
-        <img className="rectangle-icon" alt="rectangle.svg" src="/rectangle.svg" />
+        <img className="rectangle-icon" alt="rectangle.svg" src={bgImg} />
+
         <div className="logo">
           <img className="logo-icon" loading="lazy" alt="logo.svg" src={logoImg} />
           <h3 className="fba-quant">FBA Quant</h3>
