8d860bc626b6f428082585ceb266a8d585a6ef12
kjlee1203 <kjuni1@naver.com>
2024-03-10 19:04:43 +0900

    sidebar completed (except icons)

--- dashboard2/src/components/sidebar/Todo.jsx
+++ dashboard2/src/components/sidebar/Todo.jsx
@@ -1,58 +1,50 @@
 import "./Todo.css";
+import MenuButton from "./MenuButton.jsx";
 
+// icons
 import pfIcon from "../../../public/pf-icon.png";
+import alertsIcon from "../../../public/alerts-icon.png";
+import analyticsIcon from "../../../public/analytics-icon.png";
+
+// to remember the selected menu
+import { useState } from "react";
 
 const Todo = () => {
+
+  const [selectedMenu, setSelectedMenu] = useState();
+
+  const handleMenuClick = (selectedDiv) => {
+    setSelectedMenu(selectedDiv);
+    alert(selectedDiv + ' was clicked');
+    // You can add any other actions you want to perform when the div is clicked
+  };
+
+  
   return (
-    <div className="apps-pages1">
-      <div className="list5">
-        <div className="investment-portfolio">
-          <img className="list-icon" alt="" src="/vector-7.svg" />
-          <img
-            className="table-row-icon"
-            loading="lazy"
-            alt=""
-            src="/vector-8.svg"
-          />
-          <img
-            className="table-cell-icon"
-            loading="lazy"
-            alt=""
-            src="/vector-9.svg"
-          />
-          <img
-            className="chart-area-icon"
-            loading="lazy"
-            alt=""
-            src="/vector-10.svg"
-          />
-          <img className="card-container-icon" alt="" src="/vector-11.svg" />
-          <img
-            className="header-frame-icon"
-            loading="lazy"
-            alt=""
-            src="/vector-12.svg"
-          />
-        </div>
-        <div className="portfolio">Portfolio</div>
-        <img className="chevron-right-icon4" alt="" src="/chevronright.svg" />
-      </div>
-      <div className="list6">
-        <img className="bell-icon" loading="lazy" alt="" src="/bell.svg" />
-        <div className="alerts">Alerts</div>
-        <img className="chevron-right-icon5" alt="" src="/chevronright.svg" />
-      </div>
-      <div className="list7">
-        <img
-          className="vector-icon4"
-          loading="lazy"
-          alt=""
-          src="/vector-13.svg"
-        />
-        <div className="analytics">Analytics</div>
-        <img className="chevron-right-icon6" alt="" src="/chevronright.svg" />
-      </div>
-    </div>
+    <menu className="apps-pages1">
+
+      <MenuButton
+        image={pfIcon}
+        isSelected={selectedMenu === 'portfolio'}
+        onSelect={() => handleMenuClick('portfolio')}>
+        Portfolio
+      </MenuButton>
+
+      <MenuButton
+        image={alertsIcon}
+        isSelected={selectedMenu === 'alerts'}
+        onSelect={() => handleMenuClick('alerts')}>
+        Alerts
+      </MenuButton>
+
+      <MenuButton
+        image={analyticsIcon}
+        isSelected={selectedMenu === 'analytics'}
+        onSelect={() => handleMenuClick('analytics')}>
+        Analytics
+      </MenuButton>
+
+    </menu>
   );
 };
 
--- dashboard2/src/pages/Dashboard.jsx
+++ dashboard2/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import Logo from "../components/Logo";
 import MainMenu from "../components/sidebar/MainMenu";
 import Todo from "../components/sidebar/Todo";
+import Settings from "../components/sidebar/Settings";
 
 import "./Dashboard.css";
 
@@ -10,37 +11,23 @@ const Dashboard = () => {
       <div className="menu">
         <div className="menu1">
           <Logo />
+          
           <div className="section">
             <div className="main-menu">MAIN MENU</div>
           </div>
           <MainMenu />
+          
           <div className="section1">
             <div className="todo">TODO</div>
           </div>
-          <h1>AppsPages</h1>
+          <Todo />
+          
+
           <div className="section2">
             <div className="settings">SETTINGS</div>
           </div>
+          <Settings />
 
-          <div className="components">
-            <div className="list">
-              <img
-                className="user-circle-icon"
-                loading="lazy"
-                alt="usercircle.svg"
-                src="/usercircle.svg"
-              />
-              <div className="manage-account">Manage Account</div>
-              <div className="badge">
-                <div className="div">4</div>
-              </div>
-              <img
-                className="chevron-right-icon"
-                alt="chevronright.svg"
-                src="/chevronright.svg"
-              />
-            </div>
-          </div>
 
         </div>
       </div>
